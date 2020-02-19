b0508840c84ee3cdfee33e2325d149747d6651e5
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-02-18 21:44:54 -0600

    Added UpdateMovie.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import axios from 'axios';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(res => {
+        setMovies(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -14,11 +27,14 @@ const App = () => {
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+
+      <Route exact path="/" render={() =>
+      <MovieList movies={movies} setMovies={setMovies} />
+      } />
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <Movie {...props} movies={movies} setMovies={setMovies} addToSavedList={addToSavedList} />;
         }}
       />
     </>
